84472e385b8e6d36c855cd05f0a7f0040470ec11
Yee <yeechoong97@gmail.com>
2022-11-10 20:35:30 +0800

    Completed Date Component

--- src/components/TextBox.tsx
+++ src/components/TextBox.tsx
@@ -9,7 +9,7 @@ interface TextBoxProps {
 const TextBox: FunctionComponent<TextBoxProps> = ({ setAction, name, value }) => {
     return (
         <div className='flex flex-col justify-start w-1/5'>
-            <span className='text-slate-600 text-base w-1/2'>{name}</span>
+            {/* <span className='text-slate-600 text-base w-1/2'>{name}</span> */}
             <input className=" mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block rounded-md sm:text-sm focus:ring-1" type="text" value={value} onChange={setAction} />
         </div>
     )
--- src/screen/Main.tsx
+++ src/screen/Main.tsx
@@ -6,6 +6,7 @@ import TextBox from '../components/TextBox'
 import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
 import TextField from '@mui/material/TextField';
 import { Divider } from '@mui/material'
+import Header from '../components/Header'
 
 const Main = () => {
 
@@ -18,7 +19,7 @@ const Main = () => {
     };
 
     return (
-        <div className='bg-slate-50 h-screen w-screen'>
+        <div className='bg-slate-50 h-screen w-screen overflow-hidden'>
             <div className='flex h-auto w-screen space-x-8 py-5 px-5'>
                 <TextField
                     label="Name"
@@ -42,10 +43,27 @@ const Main = () => {
                     />
                 </LocalizationProvider>
                 <div className=' w-1/6 my-2'>
+                    <button className="bg-green-500 hover:bg-green-700 px-5 py-2.5 text-sm leading-5 rounded-md font-semibold text-white h-12 w-full">Generate</button>
+                </div>
+                <div className=' w-1/6 my-2'>
                     <button className="bg-sky-600 hover:bg-sky-700 px-5 py-2.5 text-sm leading-5 rounded-md font-semibold text-white h-12 w-full">Proceed</button>
                 </div>
             </div>
             <Divider />
+            <div className='flex h-screen w-screen  space-y-3 flex-col overflow-auto'>
+                <Header />
+                <div className='bg-slate-50 w-full h-auto px-3 py-2 flex items-center space-x-5'>
+                    <input className="px-1 py-2 bg-slate-50  text-right border shadow-sm border-slate-300 rounded-md" value="01/11/2011" type="text" disabled />
+                    <input className=" px-3 py-2 bg-white border-2 shadow-sm border-orange-400 focus:outline-none focus:border-orange-500 focus:ring-orange-500 block rounded-md sm:text-sm focus:ring-1" type="text" />
+                    <input className=" px-3 py-2 bg-white border-2 shadow-sm border-orange-400 focus:outline-none focus:border-orange-500 focus:ring-orange-500 block rounded-md sm:text-sm focus:ring-1" type="text" />
+                    <input className=" px-3 py-2 bg-white border-2 shadow-sm border-rose-400 focus:outline-none focus:border-rose-500 focus:ring-rose-500 block rounded-md sm:text-sm focus:ring-1" type="text" />
+                    <input className=" px-3 py-2 bg-white border-2 shadow-sm border-rose-400 focus:outline-none focus:border-rose-500 focus:ring-rose-500 block rounded-md sm:text-sm focus:ring-1" type="text" />
+                    <input className=" px-3 py-2 bg-white border-2 shadow-sm border-blue-400 focus:outline-none focus:border-blue-500 focus:ring-blue-500 block rounded-md sm:text-sm focus:ring-1" type="text" />
+                    <input className=" px-3 py-2 bg-white border-2 shadow-sm border-blue-400 focus:outline-none focus:border-blue-500 focus:ring-blue-500 block rounded-md sm:text-sm focus:ring-1" type="text" />
+                </div>
+
+            </div>
+
         </div>
     )
 }
