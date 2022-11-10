fcf3ecf28845207a904a8ba4dfbe0d2e8c58c995
Yee <yeechoong97@gmail.com>
2022-11-10 20:53:23 +0800

    Modularize the Date Component

--- src/components/TextBox.tsx
+++ src/components/TextBox.tsx
@@ -1,16 +1,28 @@
 import React, { ChangeEventHandler, FunctionComponent } from 'react'
 
-interface TextBoxProps {
+export interface TimeInputProps {
+    date: string,
+    morningStart: string,
+    morningEnd: string,
+    noonStart: string,
+    noonEnd: string,
+    nightStart: string,
+    nightEnd: string,
     setAction: ChangeEventHandler<HTMLInputElement>,
-    name: string,
-    value: string,
 }
 
-const TextBox: FunctionComponent<TextBoxProps> = ({ setAction, name, value }) => {
+const TextBox: FunctionComponent<TimeInputProps> = (props) => {
+    const { date, morningStart, morningEnd, noonStart, noonEnd, nightStart, nightEnd, setAction } = props;
+
     return (
-        <div className='flex flex-col justify-start w-1/5'>
-            {/* <span className='text-slate-600 text-base w-1/2'>{name}</span> */}
-            <input className=" mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block rounded-md sm:text-sm focus:ring-1" type="text" value={value} onChange={setAction} />
+        <div className='bg-slate-50 w-full h-auto px-3 py-2 flex items-center space-x-5'>
+            <input className="px-1 py-2 bg-slate-50  text-right border shadow-sm border-slate-300 rounded-md" value={date} type="text" disabled />
+            <input className=" px-3 py-2 bg-white border-2 shadow-sm border-orange-400 focus:outline-none focus:border-orange-500 focus:ring-orange-500 block rounded-md sm:text-sm focus:ring-1" type="text" name="morningStart" value={morningStart} onChange={setAction} />
+            <input className=" px-3 py-2 bg-white border-2 shadow-sm border-orange-400 focus:outline-none focus:border-orange-500 focus:ring-orange-500 block rounded-md sm:text-sm focus:ring-1" type="text" name="morningEnd" value={morningEnd} onChange={setAction} />
+            <input className=" px-3 py-2 bg-white border-2 shadow-sm border-rose-400 focus:outline-none focus:border-rose-500 focus:ring-rose-500 block rounded-md sm:text-sm focus:ring-1" type="text" name="noonStart" value={noonStart} onChange={setAction} />
+            <input className=" px-3 py-2 bg-white border-2 shadow-sm border-rose-400 focus:outline-none focus:border-rose-500 focus:ring-rose-500 block rounded-md sm:text-sm focus:ring-1" type="text" name="noonEnd" value={noonEnd} onChange={setAction} />
+            <input className=" px-3 py-2 bg-white border-2 shadow-sm border-blue-400 focus:outline-none focus:border-blue-500 focus:ring-blue-500 block rounded-md sm:text-sm focus:ring-1" type="text" name="nightStart" value={nightStart} onChange={setAction} />
+            <input className=" px-3 py-2 bg-white border-2 shadow-sm border-blue-400 focus:outline-none focus:border-blue-500 focus:ring-blue-500 block rounded-md sm:text-sm focus:ring-1" type="text" name="nightEnd" value={nightEnd} onChange={setAction} />
         </div>
     )
 }
--- src/screen/Main.tsx
+++ src/screen/Main.tsx
@@ -2,11 +2,12 @@ import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker/Desktop
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider/LocalizationProvider'
 import moment, { Moment } from 'moment'
 import React from 'react'
-import TextBox from '../components/TextBox'
 import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
 import TextField from '@mui/material/TextField';
 import { Divider } from '@mui/material'
 import Header from '../components/Header'
+
+
 
 const Main = () => {
 
@@ -52,16 +53,6 @@ const Main = () => {
             <Divider />
             <div className='flex h-screen w-screen  space-y-3 flex-col overflow-auto'>
                 <Header />
-                <div className='bg-slate-50 w-full h-auto px-3 py-2 flex items-center space-x-5'>
-                    <input className="px-1 py-2 bg-slate-50  text-right border shadow-sm border-slate-300 rounded-md" value="01/11/2011" type="text" disabled />
-                    <input className=" px-3 py-2 bg-white border-2 shadow-sm border-orange-400 focus:outline-none focus:border-orange-500 focus:ring-orange-500 block rounded-md sm:text-sm focus:ring-1" type="text" />
-                    <input className=" px-3 py-2 bg-white border-2 shadow-sm border-orange-400 focus:outline-none focus:border-orange-500 focus:ring-orange-500 block rounded-md sm:text-sm focus:ring-1" type="text" />
-                    <input className=" px-3 py-2 bg-white border-2 shadow-sm border-rose-400 focus:outline-none focus:border-rose-500 focus:ring-rose-500 block rounded-md sm:text-sm focus:ring-1" type="text" />
-                    <input className=" px-3 py-2 bg-white border-2 shadow-sm border-rose-400 focus:outline-none focus:border-rose-500 focus:ring-rose-500 block rounded-md sm:text-sm focus:ring-1" type="text" />
-                    <input className=" px-3 py-2 bg-white border-2 shadow-sm border-blue-400 focus:outline-none focus:border-blue-500 focus:ring-blue-500 block rounded-md sm:text-sm focus:ring-1" type="text" />
-                    <input className=" px-3 py-2 bg-white border-2 shadow-sm border-blue-400 focus:outline-none focus:border-blue-500 focus:ring-blue-500 block rounded-md sm:text-sm focus:ring-1" type="text" />
-                </div>
-
             </div>
 
         </div>
