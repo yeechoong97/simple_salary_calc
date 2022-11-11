daa300c365183dedfc0280f481600b624c36988c
Yee <yeechoong97@gmail.com>
2022-11-11 22:13:44 +0800

    Completed Row Population

--- src/components/TextBox.tsx
+++ src/components/TextBox.tsx
@@ -1,6 +1,10 @@
 import React, { ChangeEventHandler, FunctionComponent } from 'react'
 
-export interface TimeInputProps {
+interface ActionProps {
+    setAction?: ChangeEventHandler<HTMLInputElement>,
+}
+
+export interface DateTimeProps {
     date: string,
     morningStart: string,
     morningEnd: string,
@@ -8,8 +12,11 @@ export interface TimeInputProps {
     noonEnd: string,
     nightStart: string,
     nightEnd: string,
-    setAction: ChangeEventHandler<HTMLInputElement>,
 }
+
+interface TimeInputProps extends ActionProps, DateTimeProps { }
+
+
 
 const TextBox: FunctionComponent<TimeInputProps> = (props) => {
     const { date, morningStart, morningEnd, noonStart, noonEnd, nightStart, nightEnd, setAction } = props;
@@ -17,12 +24,12 @@ const TextBox: FunctionComponent<TimeInputProps> = (props) => {
     return (
         <div className='bg-slate-50 w-full h-auto px-3 py-2 flex items-center space-x-5'>
             <input className="px-1 py-2 bg-slate-50  text-right border shadow-sm border-slate-300 rounded-md" value={date} type="text" disabled />
-            <input className=" px-3 py-2 bg-white border-2 shadow-sm border-orange-400 focus:outline-none focus:border-orange-500 focus:ring-orange-500 block rounded-md sm:text-sm focus:ring-1" type="text" name="morningStart" value={morningStart} onChange={setAction} />
-            <input className=" px-3 py-2 bg-white border-2 shadow-sm border-orange-400 focus:outline-none focus:border-orange-500 focus:ring-orange-500 block rounded-md sm:text-sm focus:ring-1" type="text" name="morningEnd" value={morningEnd} onChange={setAction} />
-            <input className=" px-3 py-2 bg-white border-2 shadow-sm border-rose-400 focus:outline-none focus:border-rose-500 focus:ring-rose-500 block rounded-md sm:text-sm focus:ring-1" type="text" name="noonStart" value={noonStart} onChange={setAction} />
-            <input className=" px-3 py-2 bg-white border-2 shadow-sm border-rose-400 focus:outline-none focus:border-rose-500 focus:ring-rose-500 block rounded-md sm:text-sm focus:ring-1" type="text" name="noonEnd" value={noonEnd} onChange={setAction} />
-            <input className=" px-3 py-2 bg-white border-2 shadow-sm border-blue-400 focus:outline-none focus:border-blue-500 focus:ring-blue-500 block rounded-md sm:text-sm focus:ring-1" type="text" name="nightStart" value={nightStart} onChange={setAction} />
-            <input className=" px-3 py-2 bg-white border-2 shadow-sm border-blue-400 focus:outline-none focus:border-blue-500 focus:ring-blue-500 block rounded-md sm:text-sm focus:ring-1" type="text" name="nightEnd" value={nightEnd} onChange={setAction} />
+            <input className=" px-3 py-2 bg-white border-2 shadow-sm border-orange-400 focus:outline-none focus:border-orange-500 focus:ring-orange-500 block rounded-md sm:text-sm focus:ring-1" type="text" name={`${date}|morningStart`} value={morningStart} onChange={setAction} maxLength={4} />
+            <input className=" px-3 py-2 bg-white border-2 shadow-sm border-orange-400 focus:outline-none focus:border-orange-500 focus:ring-orange-500 block rounded-md sm:text-sm focus:ring-1" type="text" name={`${date}|morningEnd`} value={morningEnd} onChange={setAction} maxLength={4} />
+            <input className=" px-3 py-2 bg-white border-2 shadow-sm border-rose-400 focus:outline-none focus:border-rose-500 focus:ring-rose-500 block rounded-md sm:text-sm focus:ring-1" type="text" name={`${date}|noonStart`} value={noonStart} onChange={setAction} maxLength={4} />
+            <input className=" px-3 py-2 bg-white border-2 shadow-sm border-rose-400 focus:outline-none focus:border-rose-500 focus:ring-rose-500 block rounded-md sm:text-sm focus:ring-1" type="text" name={`${date}|noonEnd`} value={noonEnd} onChange={setAction} maxLength={4} />
+            <input className=" px-3 py-2 bg-white border-2 shadow-sm border-blue-400 focus:outline-none focus:border-blue-500 focus:ring-blue-500 block rounded-md sm:text-sm focus:ring-1" type="text" name={`${date}|nightStart`} value={nightStart} onChange={setAction} maxLength={4} />
+            <input className=" px-3 py-2 bg-white border-2 shadow-sm border-blue-400 focus:outline-none focus:border-blue-500 focus:ring-blue-500 block rounded-md sm:text-sm focus:ring-1" type="text" name={`${date}|nightEnd`} value={nightEnd} onChange={setAction} maxLength={4} />
         </div>
     )
 }
--- src/screen/Main.tsx
+++ src/screen/Main.tsx
@@ -1,23 +1,73 @@
 import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker/DesktopDatePicker'
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider/LocalizationProvider'
 import moment, { Moment } from 'moment'
-import React from 'react'
+import { ChangeEvent, ChangeEventHandler, EventHandler, useEffect, useState } from 'react'
 import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
 import TextField from '@mui/material/TextField';
 import { Divider } from '@mui/material'
 import Header from '../components/Header'
+import TextBox, { DateTimeProps } from '../components/TextBox';
 
-
+const InitialRecord: DateTimeProps = {
+    date: "",
+    morningStart: "",
+    morningEnd: "",
+    noonStart: "",
+    noonEnd: "",
+    nightStart: "",
+    nightEnd: "",
+}
 
 const Main = () => {
 
-    const [value, setValue] = React.useState<Moment | null>(
-        moment(new Date())
-    );
+    const [startDate, setStartDate] = useState<Moment | null>(null);
+    const [endDate, setEndDate] = useState<Moment | null>(null);
+    const [totalDays, setTotalDays] = useState<number>(0);
+    const [dateRecords, setDateRecords] = useState<DateTimeProps[]>([]);
 
-    const handleChange = (newValue: Moment | null) => {
-        setValue(newValue);
-    };
+    const populateRows = () => {
+        let differenceDays = endDate?.diff(startDate, 'days');
+        setTotalDays(differenceDays! + 1);
+    }
+
+    // Update the state of rows
+    const updateRows = (event: ChangeEvent<HTMLInputElement>) => {
+        const { value, name } = event.target;
+        const [date, fieldName] = name.split("|");
+        console.log(date);
+        console.log(fieldName);
+        const duplicateDateRecords = [...dateRecords];
+        duplicateDateRecords.find((val) => val.date === date)![fieldName as keyof DateTimeProps] = value;
+        setDateRecords(duplicateDateRecords);
+    }
+
+    useEffect(() => {
+        // Set the default date for previous month
+        const setupDate = () => {
+            const startOfMonth = moment().clone().subtract(1, 'months').startOf('month');
+            const endOfMonth = moment().clone().subtract(1, 'months').endOf('month');
+            setStartDate(startOfMonth);
+            setEndDate(endOfMonth);
+        }
+
+        // Setup All the rows initially based on number of days
+        const setupRows = () => {
+            if (totalDays !== 0) {
+                const blankRecords: DateTimeProps[] = [];
+                for (let i = 0; i < totalDays; i++) {
+                    let newRow = { ...InitialRecord, date: startDate!.add(i === 0 ? 0 : 1, "day").format("DD/MM/YYYY") };
+                    blankRecords.push(newRow);
+                }
+                setDateRecords(blankRecords);
+            }
+        }
+
+        // Setup the input rows
+        setupRows();
+
+        // Clean the function after used it
+        return () => setupDate()
+    }, [totalDays])
 
     return (
         <div className='bg-slate-50 h-screen w-screen overflow-hidden'>
@@ -25,36 +75,40 @@ const Main = () => {
                 <TextField
                     label="Name"
                     margin='dense'
-                    fullWidth
+                    className='w-1/2'
                 />
                 <LocalizationProvider dateAdapter={AdapterMoment}>
                     <DesktopDatePicker
                         label="Start Date"
                         inputFormat="MM/DD/YYYY"
-                        value={value}
-                        onChange={handleChange}
+                        value={startDate}
+                        onChange={(date) => setStartDate(date)}
                         renderInput={(params) => <TextField margin='dense' {...params} />}
                     />
                     <DesktopDatePicker
                         label="End Date"
                         inputFormat="MM/DD/YYYY"
-                        value={value}
-                        onChange={handleChange}
+                        value={endDate}
+                        onChange={(date) => setEndDate(date)}
                         renderInput={(params) => <TextField margin='dense' {...params} />}
                     />
                 </LocalizationProvider>
                 <div className=' w-1/6 my-2'>
-                    <button className="bg-green-500 hover:bg-green-700 px-5 py-2.5 text-sm leading-5 rounded-md font-semibold text-white h-12 w-full">Generate</button>
+                    <button className="bg-green-500 hover:bg-green-700 px-5 py-2.5 text-sm leading-5 rounded-md font-semibold text-white h-12 w-full" onClick={populateRows}>Generate</button>
                 </div>
                 <div className=' w-1/6 my-2'>
                     <button className="bg-sky-600 hover:bg-sky-700 px-5 py-2.5 text-sm leading-5 rounded-md font-semibold text-white h-12 w-full">Proceed</button>
                 </div>
             </div>
             <Divider />
-            <div className='flex h-screen w-screen  space-y-3 flex-col overflow-auto'>
+            <div className='flex h-full w-screen space-y-3 flex-col overflow-auto pb-32'>
                 <Header />
+                {
+                    totalDays > 0 && dateRecords.map((date, index) => (
+                        <TextBox key={index} date={date.date} morningStart={date.morningStart} morningEnd={date.morningEnd} noonStart={date.noonStart} noonEnd={date.noonEnd} nightStart={date.nightStart} nightEnd={date.nightEnd} setAction={updateRows} />
+                    ))
+                }
             </div>
-
         </div>
     )
 }
