6f9e610832e8cad4e8969977cc68b72c66ce8722
Yee <yeechoong97@gmail.com>
2022-11-13 11:50:02 +0800

    Adjust the Flexbox

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Header = () => {
     return (
-        <div className='bg-slate-200 w-full h-auto px-3 py-2 flex items-center space-x-5 sticky top-0'>
+        <div className='bg-slate-200 w-full h-auto px-3 py-2 flex items-center space-x-5 sticky top-0 justify-center'>
             <input className="uppercase mt-1 px-3 py-2 bg-slate-100 border-2 shadow-sm border-green-500 block rounded-md sm:text-sm text-right" placeholder='00:00' type="text" value="Date" disabled />
             <input className="uppercase mt-1 px-3 py-2 bg-slate-100 border-2 shadow-sm border-orange-400 block rounded-md sm:text-sm " placeholder='00:00' type="text" value="Morning (Start)" disabled />
             <input className="uppercase mt-1 px-3 py-2 bg-slate-100 border-2 shadow-sm border-orange-400 block rounded-md sm:text-sm " placeholder='00:00' type="text" value="Morning (End)" disabled />
--- src/components/TextBox.tsx
+++ src/components/TextBox.tsx
@@ -6,7 +6,7 @@ const TextBox: FunctionComponent<TimeInputProps> = (props) => {
     const { date, morningStart, morningEnd, noonStart, noonEnd, nightStart, nightEnd, setAction } = props;
 
     return (
-        <div className='bg-slate-50 w-full h-auto px-3 py-2 flex items-center space-x-5'>
+        <div className='bg-slate-50 w-full h-auto px-3 py-2 flex items-center space-x-5 justify-center'>
             <input className="px-1 py-2 bg-slate-50  text-right border shadow-sm border-slate-300 rounded-md" value={date} type="text" disabled />
             <input className=" px-3 py-2 bg-white border-2 shadow-sm border-orange-400 focus:outline-none focus:border-orange-500 focus:ring-orange-500 block rounded-md sm:text-sm focus:ring-1" type="text" name={`${date}|morningStart`} value={morningStart} onChange={setAction} maxLength={4} />
             <input className=" px-3 py-2 bg-white border-2 shadow-sm border-orange-400 focus:outline-none focus:border-orange-500 focus:ring-orange-500 block rounded-md sm:text-sm focus:ring-1" type="text" name={`${date}|morningEnd`} value={morningEnd} onChange={setAction} maxLength={4} />
